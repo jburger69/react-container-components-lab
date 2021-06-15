99bcddf20f37c7dd4d27e8461667eaf96d68dfaf
Joshua Burger <joshberger6969@outlook.com>
2021-06-14 20:01:04 -0400

    commit tree e0a9f9194d1244a10628b788f1e1fb6eb5cf5ff4

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,31 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    constructor(){
+      super();
+  
+      this.state = {
+        searchTerm: '', reviews: []
+      }
+    }
+  
+  
+    submitEvent(event) {
+      event.preventDefault();
+      fetch(URL.concat(this.state.searchTerm))
+          .then(resp => resp.json())
+          .then(resp => this.setState({ reviews: resp.results }));
+    }
+  
+    render(){
+      return(
+        <div className="searchable-movie-reviews">
+          <h3>The Searched Movie Reviews:</h3>
+          <MovieReviews reviews={this.state.reviews} />
+        </div>
+      );
+    }
+  }
+  
+export default SearchableMovieReviewsContainer;
